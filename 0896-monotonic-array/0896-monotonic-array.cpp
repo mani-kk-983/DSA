670603318e1e2b74336dd34065977593746fc9ad
Omkar K <manikanta.kk983@gmail.com>
2026-08-03 19:01:57 +0530

    Time: 0 ms (100%), Space: 100.3 MB (70.01%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -5,11 +5,10 @@ public:
         bool decreasing = true;
 
         for (int i = 0; i < nums.size() - 1; i++) {
+
             if (nums[i] > nums[i + 1])
                 increasing = false;
-        }
 
-        for (int i = 0; i < nums.size() - 1; i++) {
             if (nums[i] < nums[i + 1])
                 decreasing = false;
         }
