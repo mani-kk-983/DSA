093df2e2329b04ad57ad78bfb6f13fd24f92a868
Omkar K <manikanta.kk983@gmail.com>
2026-08-07 00:38:06 +0530

    Time: 0 ms (100%), Space: 13.4 MB (82.32%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-    // Create a result vector of size 2*n initialized to 0
-        vector<int> ans(2 * n);
-        
-        for (int i = 0; i < n; ++i) {
-            ans[2 * i] = nums[i];       // Place x_i at even index
-            ans[2 * i + 1] = nums[n + i]; // Place y_i at odd index
+    vector<int> tong;
+        for(int i=0;i<n;i++){
+            tong.push_back(nums[i]);
+             tong.push_back(nums[i+n]);
         }
+        return tong;
         
-        return ans;
     }
 };
